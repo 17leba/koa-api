0742b1b41c2b3c871f2999180872e15259adf9bb
17leba <17leba@gmail.com>
2018-01-22 14:37:03 +0800

    部署脚本修改

--- publish.sh
+++ publish.sh
@@ -1,6 +1,8 @@
-git reset --hard || { echo "command failed"; exit 1; }
+COMMIT_ID=""$1
 
-git pull origin || { echo "command failed"; exit 1; }
+git reset --hard || { echo "command failed"; exit 1; }
+git fetch -p origin || { echo "command failed"; exit 1; }
+git checkout $COMMIT_ID || { echo "command failed"; exit 1; }
 
 cd fe
 npm run build || { echo "command failed"; exit 1; }
